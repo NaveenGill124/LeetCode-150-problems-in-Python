09278e195bb2396d829fbc772f5d69418b157128
NaveenGill124 <gillnaveen121@gmail.com>
2025-07-17 18:08:01 +0530

    Adding Array problems

---
+++ Array and String/Gas Station.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+
+        if sum(gas) < sum(cost):
+            return -1
+        
+        curr_gas = 0
+        start = 0
+        for i in range(len(gas)):
+
+            curr_gas = curr_gas + gas[i] - cost[i]
+
+            if curr_gas < 0:
+
+                start = i + 1
+                curr_gas = 0
+            
+        
+        return start
+      
